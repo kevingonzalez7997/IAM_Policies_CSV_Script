e54604212e6d8486c6dc64240b4ce12d34681ff4
kevingonzalez7997 <59447523+kevingonzalez7997@users.noreply.github.com>
2023-10-20 23:36:28 -0400

    Update IAM_Policies.py

--- IAM_Policies.py
+++ IAM_Policies.py
@@ -1,6 +1,12 @@
 import csv
 import boto3
-
+import time
+import os.path
+print("Hello and Welcome to IAM Policy Printer\n")
+time.sleep(2)
+print("All IAM policies will be returned and filter\n")
+time.sleep(2)
+print("The results will be written in IAM_Policies.csv\n")
 # Create an AWS Identity and Access Management (IAM) client using Boto3
 client = boto3.client('iam')
 # Define the function
@@ -23,13 +29,24 @@ def policies_list():
             "Arn": policy['Arn']
         })
     # Write the results to the CSV file
-    # Open the csv file , set this command to csv_file
+    # Open the CSV file, set this command to csv_file
     with open(csv_filename, 'w', newline='') as csv_file:
-       #Use DictWriter as results have been stored in dictionary 
+       #Use DictWriter as results have been stored in a dictionary 
         writer = csv.DictWriter(csv_file, fieldnames=['Policy_Name', 'PolicyId', 'Arn'], extrasaction='ignore')
         # Creates a header with field names
         writer.writeheader()
-        # Writes each dictonary as a row
+        # Writes each dictionary as a row
         writer.writerows(policies_filtered)
 # Run Function
 policies_list()
+# Creates a variable for the path of the CSV file
+path = './IAM_Policies.csv'
+# Check if the file has been created
+check_file = os.path.exists(path)
+# If statement depending on the result
+if check_file == True:
+       time.sleep(2)
+       print("Everything went smoothly, you should see the IAM_Policies.csv")
+else:
+       time.sleep(2)
+       print("Error try again")
